73a77ceff8f517f672d31f576f34c47240a79e9d
neusman1 <nick.eusman@gmail.com>
2017-05-15 14:39:58 -0400

    Create Unit.java

---
+++ Unit.java
@@ -0,0 +1,12 @@
+
+public abstract class Unit extends Tile{
+
+	public Unit(char disp, boolean obst, int hit, Point pos, boolean dmg) {
+		super(disp, obst, hit, pos, dmg);
+	}
+
+	public void moveTo(Point p){
+		this.setPosition(p);
+	}
+	
+}
