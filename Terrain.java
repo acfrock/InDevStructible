5b7408f58c73444d991f69710a21ef3f97d3f578
neusman1 <nick.eusman@gmail.com>
2017-07-26 13:46:19 -0400

    added default constructor, elevation change functions

--- Terrain.java
+++ Terrain.java
@@ -33,4 +33,27 @@ public class Terrain extends Tile
         elevation = e;
     }
 
+    public Terrain(Point pos){
+    	super('_', false, 0, pos, false);
+    	cover = Cover.NO_COVER;
+    	elevation = Elevation.MEDIUM;
+    }
+    
+    public void IncreaseElevation(){
+    	if( elevation == Elevation.MEDIUM){
+    		elevation = Elevation.HIGH;
+    	}else if (elevation == Elevation.LOW){
+    		elevation = Elevation.MEDIUM;
+    	}
+    }
+    
+    public void DecreaseElevation(){
+    	if(elevation == Elevation.MEDIUM){
+    		elevation = Elevation.LOW;
+    	}else if (elevation == Elevation.HIGH){
+    		elevation = Elevation.MEDIUM;
+    	}
+    }
+    
+    
 }
